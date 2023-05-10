f7234affccd4dcc3475768a086985721e155c668
Franck Oculi <100290322+FranckOculi@users.noreply.github.com>
2023-05-10 08:46:21 +0200

    Add files via upload

---
+++ src/config/configuration.js
@@ -0,0 +1,14 @@
+// 'use strict';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const data = {
+  env: process.env.NODE_ENV || 'development',
+	jwt_secret: process.env.JWT_SECRET,
+	server_port: process.env.SERVER_PORT,
+	sqlite_path: process.env.SQLITE_FILENAME,
+	initial_admin_password: process.env.INITIAL_ADMIN_PASSWORD,
+	initial_admin_email: process.env.INITIAL_ADMIN_EMAIL,
+};
+
+export default data;
