e7ae0316a7e72245898ce0e16533e480fc9a9de4
Franck Oculi <100290322+FranckOculi@users.noreply.github.com>
2023-05-10 08:39:22 +0200

    Add files via upload

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+import configuration from './src/config/configuration.js'
+
+export default {
+	client: 'sqlite3',
+	connection:{
+		filename: configuration?.sqlite_path,
+	},
+	useNullAsDefault: true,
+	migrations: {
+		directory: './src/database/migrations',
+	},
+	seeds: {
+		directory: './src/database/seeds',
+	},
+}
