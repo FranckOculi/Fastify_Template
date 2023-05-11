cb7e79cea109a2fd909f304ea574aac175130a04
Franck Oculi <100290322+FranckOculi@users.noreply.github.com>
2023-05-11 10:25:36 +0200

    configuration update to typescript

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -5,7 +5,7 @@ dotenv.config();
 const data = {
   env: process.env.NODE_ENV || 'development',
 	jwt_secret: process.env.JWT_SECRET,
-	server_port: process.env.SERVER_PORT,
+	server_port: parseInt(process.env.SERVER_PORT),
 	sqlite_path: process.env.SQLITE_FILENAME,
 	initial_admin_password: process.env.INITIAL_ADMIN_PASSWORD,
 	initial_admin_email: process.env.INITIAL_ADMIN_EMAIL,
