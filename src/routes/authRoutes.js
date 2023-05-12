b4f9dadaf8ded2c6951781792ba7c8bc9b252e65
Franck Oculi <100290322+FranckOculi@users.noreply.github.com>
2023-05-12 16:55:52 +0200

    Create authRoutes.js

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,26 @@
+import { signUpUserSchema, loginUserSchema } from '../schemas/authSchema.js';
+import { signUp, login, requireAuth } from '../controllers/authController.js';
+
+async function authRoutes(fastify, options, done) {
+  fastify.route({
+    method: 'GET',
+    url: '/jwtid',
+    handler: requireAuth,
+  });
+
+  fastify.route({
+    method: 'POST',
+    url: '/register',
+    schema: signUpUserSchema,
+    handler: signUp,
+  });
+
+  fastify.route({
+    method: 'POST',
+    url: '/login',
+    schema: loginUserSchema,
+    handler: login,
+  });
+}
+
+export default authRoutes;
