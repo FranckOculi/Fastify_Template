58d913256511f65d3505cf6e8a41a5093d49e2d2
Chouchoubeignet <100290322+Chouchoubeignet@users.noreply.github.com>
2023-05-13 10:25:18 +0200

    database folder delete to root

--- database/connection.js
+++
@@ -1,9 +0,0 @@
-// import knexPkg from "knex";
-// const { knex } = knexPkg;
-import knex from "knex";
-
-import knexConfiguration from "../../knexfile";
-
-const database = knex(knexConfiguration);
-
-export default database;
