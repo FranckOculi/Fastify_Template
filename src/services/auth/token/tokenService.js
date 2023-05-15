75028a26cb62efe92ad1a258670921365a519602
Chouchoubeignet <100290322+Chouchoubeignet@users.noreply.github.com>
2023-05-15 07:06:00 +0200

    tokenService Update : add refreshTOkenService

--- src/services/auth/token/tokenService.js
+++ src/services/auth/token/tokenService.js
@@ -1,19 +1,34 @@
-import Token from "../../../utils/Token.js";
+import Token from '../../../utils/Token.js'
 
-export const TokenService = async (tokenObject) => {
-    try {
-        const token = tokenObject.replace('Bearer ', '');
-    
-        if (token) {
-            const decodedToken = await Token.verifyToken(token)
+export const refreshTokenService = async (tokenObject) => {
+	if (!tokenObject) return { error: 'Token required', status: 401 }
+	else {
+		try {
+			const token = tokenObject.replace('Bearer ', '')
 
-            if(!decodedToken) return {error: 'Token not valid', status: 401}
+			if (token) {
+				const decodedToken = await Token.verifyToken(token)
 
-            return {error: null, data: decodedToken}
-        } else {
-            return {error: 'Token required', status: 401}
-        }
-      } catch (err) {
-        throw new Error('Unable to verify token');
-      }
+				if (!decodedToken) return { error: 'Token not valid', status: 401 }
+
+				const accessToken = Token.createToken({
+					id: decodedToken.id,
+					email: decodedToken.email,
+					access: decodedToken.access,
+				})
+
+				return {
+					error: null,
+					data: {
+						user: decodedToken.id,
+						accessToken,
+					},
+				}
+			} else {
+				return { error: 'Token required', status: 401 }
+			}
+		} catch (err) {
+			throw new Error('Unable to verify token')
+		}
+	}
 }
