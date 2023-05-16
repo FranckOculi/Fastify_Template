a1451105406f29bbe5c4a98d7ed66c869efac5db
Chouchoubeignet <100290322+Chouchoubeignet@users.noreply.github.com>
2023-05-16 18:39:14 +0200

    configuration Upadate : add app url

--- src/config/configuration.js
+++ src/config/configuration.js
@@ -1,15 +1,15 @@
 // 'use strict';
-import dotenv from 'dotenv';
-dotenv.config();
+import dotenv from 'dotenv'
+dotenv.config()
 
 const data = {
-  env: process.env.NODE_ENV || 'development',
-	jwt_secret: process.env.JWT_SECRET,
+	env: process.env.NODE_ENV || 'development',
 	server_port: parseInt(process.env.SERVER_PORT),
+	app_url: process.env.APP_URL,
 	sqlite_path: process.env.SQLITE_FILENAME,
 	initial_admin_password: process.env.INITIAL_ADMIN_PASSWORD,
 	initial_user_password: process.env.INITIAL_USER_PASSWORD,
 	initial_admin_email: process.env.INITIAL_ADMIN_EMAIL,
-};
+}
 
-export default data;
+export default data
