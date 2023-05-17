0c5f31048223c153789b7157f8322ee7434932c4
Chouchoubeignet <100290322+Chouchoubeignet@users.noreply.github.com>
2023-05-17 07:55:21 +0200

    authController Update : add cookie

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,46 +1,46 @@
-import { registerUser } from '../services/auth/register/registerService.js';
-import { loginService } from '../services/auth/login/loginService.js';
+import { registerUser } from '../services/auth/register/registerService.js'
+import { loginService } from '../services/auth/login/loginService.js'
+import { refreshTokenService } from '../services/auth/token/tokenService.js'
 
 export const signUp = async (req, res) => {
-    const { error, status, data } = await registerUser({
-      teamId: req.body.teamId,
-      displayName: req.body.displayName,
-      email: req.body.email,
-      password: req.body.password,
-      access: req.body.access,
-      phone: req.body.phone,
-    })
+	const { error, status, data } = await registerUser({
+		teamId: req.body.teamId,
+		displayName: req.body.displayName,
+		email: req.body.email,
+		password: req.body.password,
+		access: req.body.access,
+		phone: req.body.phone,
+	})
 
-    if (error) return res.code(status).send({ message: error });
-    
-    return res.code(201).send({ 
-      message: 'User added !',
-      data
-    });
+	if (error) return res.code(status).send({ message: error })
 
+	return res.code(201).send({
+		message: 'User added !',
+		data,
+	})
 }
 
 export const login = async (req, res) => {
-  const {error, status, data } = await loginService({
-    email: req.body.email, 
-    password: req.body.password
-  })
+	const { error, status, data } = await loginService({
+		email: req.body.email,
+		password: req.body.password,
+	})
 
-  if (error) return res.code(status).send({ message: error });
+	if (error) return res.code(status).send({ message: error })
 
-  return res.code(200).send({
-    message: 'Authenticate with success !',
-    data
-});
-}
+	const dataResponse = {
+		user: data.user,
+		accessToken: data.accessToken,
+	}
 
-export const requireAuth = async (req, res) => {
-  const {error, status, data} = await TokenService(req.headers.authorization)
+	res.cookie(
+		'jwt',
+		data.refreshTokenObject.refreshToken,
+		data.refreshTokenObject.options
+	)
 
-  if(error) return res.code(status).send({ message: error });
-
-  return res.code(200).send({
-    message: 'Authenticate with success !',
-    data,
-  });
+	return res.code(200).send({
+		message: 'Authenticated with success !',
+		data: dataResponse,
+	})
 }
