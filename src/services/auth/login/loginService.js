54aa3f5633f8071967bb16390c48ecd6ef3f3705
Chouchoubeignet <100290322+Chouchoubeignet@users.noreply.github.com>
2023-05-17 07:56:31 +0200

    loginService Update : add refresh token

--- src/services/auth/login/loginService.js
+++ src/services/auth/login/loginService.js
@@ -1,37 +1,64 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcrypt'
 
-import { findByEmail } from "../../../repositories/userRepository.js";
-import Token from '../../../utils/Token.js';
+import { findByEmail } from '../../../repositories/userRepository.js'
+import Token from '../../../utils/Token.js'
 
 export const loginService = async (data) => {
-    try {
-        const user = await findByEmail(data.email, [
-          'id',
-          'teamId',
-          'email',
-          'password',
-          'access',
-        ])
+	try {
+		const user = await findByEmail(data.email, [
+			'id',
+			'teamId',
+			'email',
+			'password',
+			'access',
+		])
 
-        if (!user)
-		    return { error: "That email and password combination is incorrect", status: 401 }
-    
-        const isValidPassword = await bcrypt.compare(
-          data.password,
-          user.password,
-        );
+		if (!user)
+			return {
+				error: 'That email and password combination is incorrect',
+				status: 401,
+			}
 
-        if(!isValidPassword) return { error: "That email and password combination is incorrect", status: 401 }
+		const isValidPassword = await bcrypt.compare(data.password, user.password)
 
-        const token = Token.createToken({id: user.id, email: user.email, access: user.access});
+		if (!isValidPassword)
+			return {
+				error: 'That email and password combination is incorrect',
+				status: 401,
+			}
 
-        return {
-          error: null, data: {
-            user: user.id,
-            token,
-          }}
+		const refreshToken = Token.createToken(
+			{
+				id: user.id,
+				email: user.email,
+				access: user.access,
+			},
+			'3d'
+		)
 
-      } catch (err) {
-		throw new Error('Unable to complete signin');
+		const accessToken = Token.createToken({
+			id: user.id,
+			email: user.email,
+			access: user.access,
+		})
+
+		return {
+			error: null,
+			data: {
+				user: user.id,
+				accessToken,
+				refreshTokenObject: {
+					refreshToken,
+					options: {
+						httpOnly: true,
+						sameSite: 'None',
+						secure: true,
+						maxAge: 60 * 60 * 24 * 3,
+					},
+				},
+			},
+		}
+	} catch (err) {
+		throw new Error('Unable to complete signin')
 	}
 }
