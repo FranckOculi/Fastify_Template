4ecc4e8adf90fe797293766fd7da1e32d6fe3cf3
Chouchoubeignet <100290322+Chouchoubeignet@users.noreply.github.com>
2023-05-21 11:02:34 +0200

    meService.test Feat

---
+++ src/services/user/me/meService.test.js
@@ -0,0 +1,18 @@
+import { getMeService } from './meService'
+
+describe('getMeService', () => {
+	it('should return an error when no user', async () => {
+		const id = 25
+		const { error, status, data } = await getMeService(id)
+
+		expect(status).toBe(404)
+		expect(error).toBe("This user doesn't exist")
+	})
+	it('should succeed', async () => {
+		const id = 2
+		const { error, status, data } = await getMeService(id)
+
+		expect(data.user.id).toBe(2)
+		expect(data.team.id).toBe(2)
+	})
+})
