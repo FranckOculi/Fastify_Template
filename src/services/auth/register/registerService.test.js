ad655440c627cedcff44e02b7cafbcb970e0a579
Chouchoubeignet <100290322+Chouchoubeignet@users.noreply.github.com>
2023-05-22 19:50:18 +0200

    registerService.test Feat

---
+++ src/services/auth/register/registerService.test.js
@@ -0,0 +1,81 @@
+import { registerTeam, registerUser } from './registerService'
+import { findByEmail, removeUser } from '../../../repositories/userRepository'
+import { findByName, removeTeam } from '../../../repositories/teamRepository'
+
+describe('registerService', () => {
+	const newUser = {
+		teamId: 2,
+		displayName: 'new-user_registerService',
+		email: `new-user_registerService@test.com`,
+		password: 'new-user',
+	}
+
+	const newUserError = {
+		teamId: 1,
+		displayName: 'John Wayne',
+		email: `johnwayne@admin.fr`,
+		password: 'user',
+	}
+
+	const newTeam = {
+		id: 3,
+		name: 'new-team_registerService',
+		description: 'new-team',
+	}
+
+	const newTeamError = {
+		id: 1,
+		name: 'Team React',
+		description: 'Groupe de travail',
+	}
+
+	beforeEach(async () => {
+		const user = await findByEmail(newUser.email)
+		const team = await findByName(newTeam.name)
+
+		if (user) await removeUser(user.id)
+		if (team) await removeTeam(user.id)
+	})
+
+	describe('registerUser', () => {
+		it("should return an error when team doesn't exist", async () => {
+			const { error, status, data } = await registerUser({
+				...newUserError,
+				teamId: 3,
+			})
+
+			expect(status).toBe(404)
+			expect(error).toBe("This team doesn't exist")
+		})
+
+		it('should return an error when email is already taken', async () => {
+			const { error, status, data } = await registerUser(newUserError)
+
+			expect(status).toBe(409)
+			expect(error).toBe('This email is already taken')
+		})
+
+		it('should succeed', async () => {
+			const { error, status, data } = await registerUser(newUser)
+
+			expect(error).toBeNull()
+			expect(data.displayName).toBe(newUser.displayName)
+		})
+	})
+
+	describe('registerTeam', () => {
+		it('should return an error when team already exist', async () => {
+			const { error, status, data } = await registerTeam(newTeamError)
+
+			expect(status).toBe(409)
+			expect(error).toBe('This name is already taken')
+		})
+
+		it('should succeed', async () => {
+			const { error, status, data } = await registerTeam(newTeam)
+
+			expect(error).toBeNull()
+			expect(data.name).toBe(newTeam.name)
+		})
+	})
+})
