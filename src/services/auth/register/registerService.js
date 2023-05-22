951c424d2321fded4296748b60d97d5ce3f242b9
Chouchoubeignet <100290322+Chouchoubeignet@users.noreply.github.com>
2023-05-22 19:49:57 +0200

    registerService Update : modify param in registerTeam function

--- src/services/auth/register/registerService.js
+++ src/services/auth/register/registerService.js
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcrypt'
 
 import {
 	createTeam,
@@ -10,35 +10,34 @@ import {
 	findByEmail,
 } from '../../../repositories/userRepository.js'
 
-export const registerUser = async ( data ) => {
+export const registerUser = async (data) => {
 	const team = await findById(data.teamId)
-	if (!team)
-		return { error: "This team doesn't exist", status: 404 }
+	if (!team) return { error: "This team doesn't exist", status: 404 }
 
 	const user = await findByEmail(data.email)
 	if (user) return { error: 'This email is already taken', status: 409 }
 
 	try {
-		const salt = await bcrypt.genSalt();
-		const hash = await bcrypt.hash(data.password, salt);
+		const salt = await bcrypt.genSalt()
+		const hash = await bcrypt.hash(data.password, salt)
 
 		const newUser = await createUser({
 			teamId: data.teamId,
 			displayName: data.displayName,
 			email: data.email,
 			password: hash,
-			access: data.access ?? "staff",
+			access: data.access ?? 'staff',
 			phone: data.phone,
-			createdAt: new Date()
-		  })
+			createdAt: new Date(),
+		})
 
 		return { error: null, data: newUser }
 	} catch (err) {
-		throw new Error('Unable to complete signup');
+		throw new Error('Unable to complete signup')
 	}
 }
 
-export const registerTeam = async ({ data }) => {
+export const registerTeam = async (data) => {
 	const team = await findByName(data.name)
 	if (team) return { error: 'This name is already taken', status: 409 }
 
