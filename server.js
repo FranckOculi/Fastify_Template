f747f74a7c00cf744cb2b72eb9e10727123deb5c
Chouchoubeignet <100290322+Chouchoubeignet@users.noreply.github.com>
2023-05-25 19:21:05 +0200

    server Update : forward logic to app function

--- server.js
+++ server.js
@@ -1,46 +1,12 @@
-import fastify from 'fastify'
-import cors from '@fastify/cors'
-import cookie from '@fastify/cookie'
-
+import App from './app.js'
 import configuration from './src/config/configuration.js'
-import authRoutes from './src/routes/authRoutes.js'
-import userRoutes from './src/routes/userRoutes.js'
-
-const app = fastify({
-	logger: true,
-})
-
-// cors
-app.register(cors, {
-	origin: true,
-	// origin: configuration.app_url,
-	// allowedHeaders: [
-	//   'Origin',
-	//   'X-Requested-With',
-	//   'Accept',
-	//   'Content-Type',
-	//   'Authorization',
-	// ],
-	// methods: ['GET', 'PUT', 'POST', 'DELETE'],
-})
-
-// cookie
-app.register(cookie, {
-	secret: configuration.token_secret,
-	hook: 'onRequest',
-	parseOptions: {},
-})
-
-// Routes managers
-app.register(authRoutes, { prefix: '/auth' })
-app.register(userRoutes, { prefix: '/user' })
 
 // Run the server!
 const start = async () => {
 	try {
-		await app.listen({ port: configuration.server_port })
+		await App.listen({ port: configuration.server_port })
 	} catch (err) {
-		app.log.error(err)
+		App.log.error(err)
 		process.exit(1)
 	}
 }
