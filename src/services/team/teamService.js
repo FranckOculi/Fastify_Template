514c211be747a53529ae45d965058262530484c3
Chouchoubeignet <100290322+Chouchoubeignet@users.noreply.github.com>
2023-05-25 19:32:11 +0200

    teamService Update : delete error response

--- src/services/team/teamService.js
+++ src/services/team/teamService.js
@@ -1,4 +1,4 @@
-import { createTeam } from "../../repositories/teamRepository.js"
+import { createTeam } from '../../repositories/teamRepository.js'
 
 export const createTeamService = async (data) => {
 	data.createdAt = Date.now()
@@ -6,6 +6,5 @@ export const createTeamService = async (data) => {
 
 	const response = await createTeam(data)
 
-	if (response) return { error: null, data: response }
-	return { error: response }
+	return { error: null, data: response }
 }
