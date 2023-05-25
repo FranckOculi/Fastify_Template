1904962829d5b80c016672ac0120e1211840fba8
Chouchoubeignet <100290322+Chouchoubeignet@users.noreply.github.com>
2023-05-25 19:21:23 +0200

    app Feat

---
+++ app.js
@@ -0,0 +1,38 @@
+import fastify from 'fastify'
+import cors from '@fastify/cors'
+import cookie from '@fastify/cookie'
+
+import configuration from './src/config/configuration.js'
+import authRoutes from './src/routes/authRoutes.js'
+import userRoutes from './src/routes/userRoutes.js'
+
+const app = fastify({
+	logger: configuration.env === 'test' ? false : true,
+})
+
+// cors
+app.register(cors, {
+	origin: true,
+	// origin: configuration.app_url,
+	// allowedHeaders: [
+	//   'Origin',
+	//   'X-Requested-With',
+	//   'Accept',
+	//   'Content-Type',
+	//   'Authorization',
+	// ],
+	// methods: ['GET', 'PUT', 'POST', 'DELETE'],
+})
+
+// cookie
+app.register(cookie, {
+	secret: configuration.token_secret,
+	hook: 'onRequest',
+	parseOptions: {},
+})
+
+// Routes managers
+app.register(authRoutes, { prefix: '/auth' })
+app.register(userRoutes, { prefix: '/user' })
+
+export default app
