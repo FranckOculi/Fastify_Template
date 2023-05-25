bbd02d4f4c164857bba7e665f6f42b84630c8b2b
Chouchoubeignet <100290322+Chouchoubeignet@users.noreply.github.com>
2023-05-25 18:53:06 +0200

    teamService.test Feat

--- src/services/team/teamService.test.js
+++ src/services/team/teamService.test.js
@@ -1,3 +1,9 @@
+import {
+	createTeam,
+	findById,
+	findByName,
+	removeTeam,
+} from '../../repositories/teamRepository'
 import { createTeamService } from './teamService'
 
 describe('createTeamService', () => {
@@ -6,19 +12,45 @@ describe('createTeamService', () => {
 		description: 'test',
 	}
 	const newTeamError = {
-		id: 2,
 		name: 'Team Test',
 		description: 'test',
 	}
+
+	let errorTeamId
+
+	beforeAll(async () => {
+		const teamError = await findByName(newTeamError.name)
+
+		if (!teamError) {
+			newTeamError.createdAt = Date.now()
+			newTeamError.updatedAt = Date.now()
+
+			const newTeam = await createTeam(newTeamError)
+			errorTeamId = newTeam.id
+		}
+	})
+
+	beforeEach(async () => {
+		const team = await findByName(newTeam.name)
+
+		if (team) await removeTeam(team.id)
+	})
+
+	afterEach(async () => {
+		const team = await findByName(newTeam.name)
+
+		if (team) await removeTeam(team.id)
+	})
+
+	afterAll(async () => {
+		const teamError = await findByName(newTeamError.name)
+
+		if (teamError) await removeTeam(newTeamError.id)
+	})
 
 	it('should return an error when team already exist', async () => {
 		const { error, status, data } = await createTeamService(newTeamError)
 
 		expect(error).toBeDefined()
-	})
-	it('should succeed', async () => {
-		const { error, status, data } = await createTeamService(newTeam)
-
-		expect(data.name).toBe(newTeam.name)
 	})
 })
