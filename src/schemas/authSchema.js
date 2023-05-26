bbe89ed4a92ce1f25ae3411fe13c757ea970e0f3
Chouchoubeignet <100290322+Chouchoubeignet@users.noreply.github.com>
2023-05-26 06:51:11 +0200

    signUp scheama Update : add teamId property

--- src/schemas/authSchema.js
+++ src/schemas/authSchema.js
@@ -1,26 +1,27 @@
 const bodyJsonSchemaSignUp = {
-  type: 'object',
-  required: ['displayName', 'email', 'password'],
-  properties: {
-    displayName: { type: 'string', minLength: 5, maxLength: 25 },
-    email: { type: 'string', maxLength: 50 },
-    password: { type: 'string', maxLength: 50 },
-  },
-};
+	type: 'object',
+	required: ['teamId', 'displayName', 'email', 'password'],
+	properties: {
+		teamId: { type: 'number' },
+		displayName: { type: 'string', minLength: 5, maxLength: 25 },
+		email: { type: 'string', maxLength: 50 },
+		password: { type: 'string', maxLength: 50 },
+	},
+}
 
 const bodyJsonSchemaLogin = {
-  type: 'object',
-  required: ['email', 'password'],
-  properties: {
-    email: { type: 'string', maxLength: 50 },
-    password: { type: 'string', maxLength: 50 },
-  },
-};
+	type: 'object',
+	required: ['email', 'password'],
+	properties: {
+		email: { type: 'string', maxLength: 50 },
+		password: { type: 'string', maxLength: 50 },
+	},
+}
 
 export const signUpUserSchema = {
-  body: bodyJsonSchemaSignUp,
-};
+	body: bodyJsonSchemaSignUp,
+}
 
 export const loginUserSchema = {
-  body: bodyJsonSchemaLogin,
-};
+	body: bodyJsonSchemaLogin,
+}
