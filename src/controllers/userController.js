26fba96a6ea977dd9c38b77b0a5d159c301f6044
Chouchoubeignet <100290322+Chouchoubeignet@users.noreply.github.com>
2023-05-26 06:42:43 +0200

    userController Update getAllUsers: delete return error

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,53 +1,57 @@
-import { fetchAll } from '../services/user/multiple/multipleService.js';
-import { deleteUserService, getUserService, updateUserService } from '../services/user/single/singleService.js';
-import {getMeService} from '../services/user/me/meService.js'
+import { fetchAll } from '../services/user/multiple/multipleService.js'
+import {
+	deleteUserService,
+	getUserService,
+	updateUserService,
+} from '../services/user/single/singleService.js'
+import { getMeService } from '../services/user/me/meService.js'
 
 export const getSingleUser = async (req, res) => {
-    const {error, status, data} = await getUserService(req.params.id)
+	const { error, status, data } = await getUserService(req.params.id)
 
-    if(error) return res.code(status).send({ message: error });
+	if (error) return res.code(status).send({ message: error })
 
-    return res.code(200).send({
-      message: 'User data',
-      data
-    });
+	return res.code(200).send({
+		message: 'User data',
+		data,
+	})
 }
 
 export const getAllUsers = async (req, res) => {
-  const {error, status, data } = fetchAll()
+	const { error, status, data } = await fetchAll()
 
-  if(error) return res.code(status).send({ message: error });
-
-  return res.code(200).send({
-    message: 'All users',
-    data
-  });
+	return res.code(200).send({
+		message: 'All users',
+		data,
+	})
 }
 
 export const updateUser = async (req, res) => {
-    const {error, status, data} = updateUserService(req.params.id)
+	const { error, status, data } = await updateUserService(req.params.id)
 
-    if(error) return res.code(status).send({ message: error });
+	if (error) return res.code(status).send({ message: error })
 
-    return res.code(201).send({ 
-      message: 'User updated !',
-      data 
-    });
+	return res.code(201).send({
+		message: 'User updated !',
+		data,
+	})
 }
 
 export const deleteUser = async (req, res) => {
-  const {error, status, data} = await deleteUserService(req.params.id)
+	const { error, status, data } = await deleteUserService(req.params.id)
 
-  return res.code(200).send({ message: 'User deleted !' });
+	if (error) return res.code(status).send({ message: error })
+
+	return res.code(200).send({ message: 'User deleted !' })
 }
 
 export const getMe = async (req, res) => {
-  const {error, status, data} = await getMeService(req.params.id)
-   
-  if(error) return res.code(status).send({ message: error });
-    
-  return res.code(200).send({
-    message: 'me',
-    data
-  });
+	const { error, status, data } = await getMeService(req.params.id)
+
+	if (error) return res.code(status).send({ message: error })
+
+	return res.code(200).send({
+		message: 'me',
+		data,
+	})
 }
