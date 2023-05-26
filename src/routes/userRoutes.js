06ba7badd58d277ebd3619f59a3942ce6817e632
Chouchoubeignet <100290322+Chouchoubeignet@users.noreply.github.com>
2023-05-26 06:50:13 +0200

    userRoutes Update : add preValidation to updateUserRoute

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,61 +1,60 @@
 import {
-  getSingleUser,
-  getAllUsers,
-  deleteUser,
-  updateUser,
-  getMe,
-} from '../controllers/userController.js';
+	getSingleUser,
+	getAllUsers,
+	deleteUser,
+	updateUser,
+	getMe,
+} from '../controllers/userController.js'
 import {
-  getUserSchema,
-  deleteUserSchema,
-  updateUserSchema,
-} from '../schemas/userSchema.js';
-import { tokenVerification } from '../middlewares/authenticationMiddleware.js';
+	getUserSchema,
+	deleteUserSchema,
+	updateUserSchema,
+} from '../schemas/userSchema.js'
+import { tokenVerification } from '../middlewares/authenticationMiddleware.js'
 import { permissionVerification } from '../middlewares/permissionMiddleware.js'
 
 async function userRoutes(fastify, options, done) {
-  fastify.route({
-    method: 'GET',
-    url: '/:id',
-    schema: getUserSchema,
-    preValidation: tokenVerification, 
-    preHandler: permissionVerification,
-    handler: getSingleUser,
-  });
+	fastify.route({
+		method: 'GET',
+		url: '/:id',
+		schema: getUserSchema,
+		preValidation: tokenVerification,
+		preHandler: permissionVerification,
+		handler: getSingleUser,
+	})
 
-  fastify.route({
-    method: 'GET', 
-    url: '/all',
-    preValidation: tokenVerification,
-    handler: getAllUsers,
-  })
+	fastify.route({
+		method: 'GET',
+		url: '/all',
+		preValidation: tokenVerification,
+		handler: getAllUsers,
+	})
 
-  fastify.route({
-    method: 'PUT',
-    url: '/update/:id',
-    schema: updateUserSchema,
-    preValidation:tokenVerification, 
-    preHandler: permissionVerification,
-    handler: updateUser,
-  });
+	fastify.route({
+		method: 'PUT',
+		url: '/update/:id',
+		schema: updateUserSchema,
+		preValidation: tokenVerification,
+		preHandler: permissionVerification,
+		handler: updateUser,
+	})
 
-  fastify.route({
-    method: 'DELETE',
-    url: '/delete/:id',
-    schema: deleteUserSchema,
-    preValidation: tokenVerification, 
-    preHandler: permissionVerification,
-    handler: deleteUser,
-  });
+	fastify.route({
+		method: 'DELETE',
+		url: '/delete/:id',
+		schema: deleteUserSchema,
+		preValidation: tokenVerification,
+		preHandler: permissionVerification,
+		handler: deleteUser,
+	})
 
-  fastify.route({
-    method: 'GET',
-    url: '/me/:id',
-    schema: getUserSchema,
-    preValidation: tokenVerification,
-    preHandler: permissionVerification,
-    handler: getMe,
-  });
-
+	fastify.route({
+		method: 'GET',
+		url: '/me/:id',
+		schema: getUserSchema,
+		preValidation: tokenVerification,
+		preHandler: permissionVerification,
+		handler: getMe,
+	})
 }
-export default userRoutes;
+export default userRoutes
