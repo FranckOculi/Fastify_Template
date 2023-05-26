7010ab2963b805a5543bd87ba22e5dca81dc5d44
Chouchoubeignet <100290322+Chouchoubeignet@users.noreply.github.com>
2023-05-26 06:47:46 +0200

    userController.test Update : add getMe

--- src/controllers/userController.test.js
+++ src/controllers/userController.test.js
@@ -384,4 +384,114 @@ describe('userController', () => {
 			})
 		})
 	})
+
+	describe('getMe', () => {
+		describe('Route protections', () => {
+			it('should return an error when no token object', async () => {
+				const response = await app.inject({
+					method: 'GET',
+					url: '/user/me/' + randomId,
+				})
+
+				const body = JSON.parse(response.body)
+				expect(response.statusCode).toEqual(401)
+				expect(body.message).toEqual('Token required')
+			})
+
+			it('should return an error when no token', async () => {
+				const response = await app.inject({
+					method: 'GET',
+					url: '/user/me/' + randomId,
+					headers: { authorization: 'Bearer ' },
+				})
+
+				const body = JSON.parse(response.body)
+				expect(response.statusCode).toEqual(401)
+				expect(body.message).toEqual('Token required')
+			})
+
+			it('should return an error when token is not valid', async () => {
+				const response = await app.inject({
+					method: 'GET',
+					url: '/user/me/' + randomId,
+					headers: { authorization: 'Bearer ' + {} },
+				})
+
+				const body = JSON.parse(response.body)
+				expect(response.statusCode).toEqual(401)
+				expect(body.message).toEqual('Token not valid')
+			})
+
+			it('should return an error because user id is different than target id', async () => {
+				const response = await app.inject({
+					method: 'GET',
+					url: '/user/me/' + randomId,
+					headers: { authorization: 'Bearer ' + newUserAccessToken },
+				})
+
+				const body = JSON.parse(response.body)
+				expect(response.statusCode).toEqual(401)
+				expect(body.message).toEqual('Error token')
+			})
+		})
+
+		describe('Route mandatory fields', () => {
+			it('should return an error when no id in param', async () => {
+				const response = await app.inject({
+					method: 'GET',
+					url: '/user/me/',
+					headers: { authorization: 'Bearer ' + newUserAccessToken },
+				})
+
+				const body = JSON.parse(response.body)
+				expect(response.statusCode).toEqual(400)
+				expect(body.message).toEqual('params/id must be number')
+			})
+		})
+
+		describe('Route errors', () => {
+			it("should return an error when user doesn't exist", async () => {
+				await removeUser(newUserId)
+				const response = await app.inject({
+					method: 'GET',
+					url: '/user/me/' + newUserId,
+					headers: { authorization: 'Bearer ' + newUserAccessToken },
+				})
+
+				const body = JSON.parse(response.body)
+				expect(response.statusCode).toEqual(404)
+				expect(body.message).toEqual("This user doesn't exist")
+			})
+		})
+
+		describe('Route success', () => {
+			it('should return user data because user is admin', async () => {
+				const response = await app.inject({
+					method: 'GET',
+					url: '/user/me/' + newUserId,
+					headers: { authorization: 'Bearer ' + newAdminAccessToken },
+				})
+
+				const body = JSON.parse(response.body)
+				expect(response.statusCode).toEqual(200)
+				expect(body.message).toEqual('me')
+				expect(body.data.user.id).toEqual(newUserId)
+				expect(body.data.team.id).toEqual(newUser.teamId)
+			})
+
+			it('should return user data because user id is the same as target id', async () => {
+				const response = await app.inject({
+					method: 'GET',
+					url: '/user/me/' + newUserId,
+					headers: { authorization: 'Bearer ' + newUserAccessToken },
+				})
+
+				const body = JSON.parse(response.body)
+				expect(response.statusCode).toEqual(200)
+				expect(body.message).toEqual('me')
+				expect(body.data.user.id).toEqual(newUserId)
+				expect(body.data.team.id).toEqual(newUser.teamId)
+			})
+		})
+	})
 })
