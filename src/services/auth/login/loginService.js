6d72eebb5ec56412adc4c7bd1b57958930ee814a
Chouchoubeignet <100290322+Chouchoubeignet@users.noreply.github.com>
2023-05-27 11:40:15 +0200

    loginService Update : delete try catch

--- src/services/auth/login/loginService.js
+++ src/services/auth/login/loginService.js
@@ -4,61 +4,57 @@ import { findByEmail } from '../../../repositories/userRepository.js'
 import Token from '../../../utils/Token.js'
 
 export const loginService = async (data) => {
-	try {
-		const user = await findByEmail(data.email, [
-			'id',
-			'teamId',
-			'email',
-			'password',
-			'access',
-		])
+	const user = await findByEmail(data.email, [
+		'id',
+		'teamId',
+		'email',
+		'password',
+		'access',
+	])
 
-		if (!user)
-			return {
-				error: 'That email and password combination is incorrect',
-				status: 401,
-			}
+	if (!user)
+		return {
+			error: 'That email and password combination is incorrect',
+			status: 401,
+		}
 
-		const isValidPassword = await bcrypt.compare(data.password, user.password)
+	const isValidPassword = await bcrypt.compare(data.password, user.password)
 
-		if (!isValidPassword)
-			return {
-				error: 'That email and password combination is incorrect',
-				status: 401,
-			}
+	if (!isValidPassword)
+		return {
+			error: 'That email and password combination is incorrect',
+			status: 401,
+		}
 
-		const refreshToken = Token.createToken(
-			{
-				id: user.id,
-				email: user.email,
-				access: user.access,
-			},
-			'3d'
-		)
-
-		const accessToken = Token.createToken({
+	const refreshToken = Token.createToken(
+		{
 			id: user.id,
 			email: user.email,
 			access: user.access,
-		})
+		},
+		'3d'
+	)
 
-		return {
-			error: null,
-			data: {
-				user: user.id,
-				accessToken,
-				refreshTokenObject: {
-					refreshToken,
-					options: {
-						httpOnly: true,
-						sameSite: 'None',
-						secure: true,
-						maxAge: 60 * 60 * 24 * 3,
-					},
+	const accessToken = Token.createToken({
+		id: user.id,
+		email: user.email,
+		access: user.access,
+	})
+
+	return {
+		error: null,
+		data: {
+			user: user.id,
+			accessToken,
+			refreshTokenObject: {
+				refreshToken,
+				options: {
+					httpOnly: true,
+					sameSite: 'None',
+					secure: true,
+					maxAge: 60 * 60 * 24 * 3,
 				},
 			},
-		}
-	} catch (err) {
-		throw new Error('Unable to complete signin')
+		},
 	}
 }
