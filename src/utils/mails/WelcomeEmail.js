609b68ef1f306b8b975fcefe26ef13586ff9e4d4
Chouchoubeignet <100290322+Chouchoubeignet@users.noreply.github.com>
2023-05-28 14:45:11 +0200

    WelcomeEmail Update : add getEmail method

--- src/utils/mails/WelcomeEmail.js
+++ src/utils/mails/WelcomeEmail.js
@@ -26,4 +26,20 @@ export default class WelcomeEmail {
 	getHtml() {
 		return '<b>Hello world?</b>'
 	}
+
+	async getEmail() {
+		if (!this.user?.displayName || !this.user?.email)
+			return { error: 'Unable to send an email', status: 503 }
+
+		return {
+			error: null,
+			message: {
+				from: this.getFrom(),
+				to: this.getReceiver(),
+				subject: this.getSubject(),
+				text: this.getText(),
+				html: this.getHtml(),
+			},
+		}
+	}
 }
