aebda34b1b085249eceb82cbfda4bfe08b17cd7b
Chouchoubeignet <100290322+Chouchoubeignet@users.noreply.github.com>
2023-05-29 18:40:52 +0200

    configuration Update : add sqlite_path_migrations variable

--- src/config/configuration.js
+++ src/config/configuration.js
@@ -8,6 +8,7 @@ const data = {
 	server_port: parseInt(process.env.SERVER_PORT),
 	app_url: process.env.APP_URL,
 	sqlite_path: process.env.SQLITE_FILENAME,
+	sqlite_path_migrations: process.env.SQLITE_FILENAME_MIGRATIONS,
 	initial_admin_password: process.env.INITIAL_ADMIN_PASSWORD,
 	initial_user_password: process.env.INITIAL_USER_PASSWORD,
 	initial_admin_email: process.env.INITIAL_ADMIN_EMAIL,
