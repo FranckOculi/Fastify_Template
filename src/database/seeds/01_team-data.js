8bae8db07036231223c412c25eeb39da70b2eab9
Chouchoubeignet <100290322+Chouchoubeignet@users.noreply.github.com>
2023-05-30 18:51:11 +0200

    team-data Update : change db access to MigrationsRepository

--- src/database/seeds/01_team-data.js
+++ src/database/seeds/01_team-data.js
@@ -1,23 +1,36 @@
-import { findById } from '../../repositories/teamRepository.js'
-import { createTeamService } from '../../services/team/teamService.js'
+import MigrationsRepository from '../migrations.config/MigrationsRepository.js'
 
 export const seed = async () => {
 	// create team if it doesn't exist
 
-	const team1 = await findById(1)
+	const repository = new MigrationsRepository('teams')
+	const teamSafeFields = ['id', 'name', 'description', 'updatedAt', 'createdAt']
+
+	const team1 = await repository.findOne({
+		where: { id: 1 },
+		select: teamSafeFields,
+	})
+
 	if (!team1) {
-		await createTeamService({
+		await repository.create({
 			id: 1,
 			name: 'Team React',
 			description: 'Groupe de travail',
+			createdAt: Date.now(),
 		})
 	}
 
-	const team2 = await findById(2)
+	const team2 = await repository.findOne({
+		where: { id: 2 },
+		select: teamSafeFields,
+	})
+
 	if (!team2) {
-		await createTeamService({
+		await repository.create({
+			id: 2,
 			name: 'Groupe GitHub',
 			description: 'Groupe de présentation des projets',
+			createdAt: Date.now(),
 		})
 	}
 
