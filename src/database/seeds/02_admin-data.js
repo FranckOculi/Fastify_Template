a7d995605f3c21a5220293a9234180fd7ef47ae3
Chouchoubeignet <100290322+Chouchoubeignet@users.noreply.github.com>
2023-05-30 20:51:49 +0200

    admin-data Update : change db access to MigrationsRepository

--- src/database/seeds/02_admin-data.js
+++ src/database/seeds/02_admin-data.js
@@ -1,41 +1,63 @@
-import configuration from "../../config/configuration.js";
-import { findByEmail } from "../../repositories/userRepository.js";
-import { registerUser } from "../../services/auth/register/registerService.js";
-import { updateUserService } from "../../services/user/single/singleService.js";
+import configuration from '../../config/configuration'
+import MigrationsRepository from '../migrations.config/MigrationsRepository'
 
 export const seed = async () => {
-  // create admin user if it doesn't exist
+	// create admin user if it doesn't exist
 
-  const userAdmin1 = await findByEmail("johnwayne@admin.fr");
-  if (!userAdmin1) {
-    await registerUser({
-      teamId: 1,
-      displayName: "John Wayne",
-      email: "johnwayne@admin.fr",
-      password: configuration.initial_admin_password,
-      access: "admin",
-      phone: "0607080910",
-    });
-  }
+	const repository = new MigrationsRepository('users')
+	const userSafeFields = [
+		'id',
+		'teamId',
+		'displayName',
+		'email',
+		'phone',
+		'access',
+		'imageUrl',
+		'updatedAt',
+		'createdAt',
+	]
 
-  if (userAdmin1 && userAdmin1.access !== "admin") {
-    await updateUserService(userAdmin1.id, {
-      access: "admin",
-      displayName: "John Wayne",
-      email: "johnwayne@admin.fr",
-    });
-  }
+	const userAdmin1 = await repository.findOne({
+		where: { email: 'johnwayne@admin.fr' },
+		select: userSafeFields,
+	})
 
-  const userAdmin2 = await findByEmail("chouchou@admin.fr");
-  if (!userAdmin2) {
-    await registerUser({
-      teamId: 2,
-      displayName: "Chouchou Beignet",
-      email: "chouchou@admin.fr",
-      password: configuration.initial_admin_password,
-      access: "admin",
-      phone: "0708091011",
-    });
-  }
-  return null;
-};
+	if (!userAdmin1) {
+		await repository.create({
+			teamId: 1,
+			displayName: 'John Wayne',
+			email: 'johnwayne@admin.fr',
+			password: configuration.initial_admin_password,
+			access: 'admin',
+			phone: '0607080910',
+			createdAt: Date.now(),
+		})
+	}
+
+	if (userAdmin1 && userAdmin1.access !== 'admin') {
+		await repository.update(userAdmin1.id, {
+			access: 'admin',
+			displayName: 'John Wayne',
+			email: 'johnwayne@admin.fr',
+			updatedAt: Date.now(),
+		})
+	}
+
+	const userAdmin2 = await repository.findOne({
+		where: { email: 'chouchou@admin.fr' },
+		select: userSafeFields,
+	})
+
+	if (!userAdmin2) {
+		await repository.create({
+			teamId: 2,
+			displayName: 'Chouchou Beignet',
+			email: 'chouchou@admin.fr',
+			password: configuration.initial_admin_password,
+			access: 'admin',
+			phone: '0708091011',
+			createdAt: Date.now(),
+		})
+	}
+	return null
+}
