e57e7cf58947da05dc7a3cea05dccfd73c969704
Chouchoubeignet <100290322+Chouchoubeignet@users.noreply.github.com>
2023-05-30 20:52:17 +0200

    user-data Update : change db access to MigrationsRepository

--- src/database/seeds/03_user-data.js
+++ src/database/seeds/03_user-data.js
@@ -1,43 +1,83 @@
-import configuration from "../../config/configuration.js";
+import configuration from '../../config/configuration'
 
-import { registerUser } from "../../services/auth/register/registerService.js";
+import MigrationsRepository from '../migrations.config/MigrationsRepository'
 
 export const seed = async () => {
-  if (configuration.env === "production") {
-    return false;
-  }
+	if (configuration.env === 'production') {
+		return false
+	}
 
-  await registerUser({
-    teamId: 1,
-    displayName: "John Doe",
-    email: "a@a.com",
-    password: configuration.initial_user_password,
-    access: "manager",
-  });
+	const repository = new MigrationsRepository('users')
+	const userSafeFields = [
+		'id',
+		'teamId',
+		'displayName',
+		'email',
+		'phone',
+		'access',
+		'imageUrl',
+		'updatedAt',
+		'createdAt',
+	]
 
-  await registerUser({
-    teamId: 2,
-    displayName: "Jane Doe",
-    email: "b@b.com",
-    password: configuration.initial_user_password,
-    access: "manager",
-  });
+	const user1 = await repository.findOne({
+		where: { email: 'a@a.com' },
+		select: userSafeFields,
+	})
 
-  for (let i = 0; i < 5; i++) {
-    await registerUser({
-      teamId: 1,
-      displayName: `randomUser${i}_team1`,
-      email: `randomUser${i}@team1.com`,
-      password: configuration.initial_user_password,
-    });
-  }
+	if (!user1)
+		await repository.create({
+			teamId: 1,
+			displayName: 'John Doe',
+			email: 'a@a.com',
+			password: configuration.initial_user_password,
+			access: 'manager',
+			createdAt: Date.now(),
+		})
 
-  for (let i = 0; i < 5; i++) {
-    await registerUser({
-      teamId: 2,
-      displayName: `randomUser${i}_team2`,
-      email: `randomUser${i}@team2.com`,
-      password: configuration.initial_user_password,
-    });
-  }
-};
+	const user2 = await repository.findOne({
+		where: { email: 'b@b.com' },
+		select: userSafeFields,
+	})
+
+	if (!user2)
+		await repository.create({
+			teamId: 2,
+			displayName: 'Jane Doe',
+			email: 'b@b.com',
+			password: configuration.initial_user_password,
+			access: 'manager',
+			createdAt: Date.now(),
+		})
+
+	for (let i = 0; i < 5; i++) {
+		const user = await repository.findOne({
+			where: { email: '`randomUser${i}@team1.com`' },
+			select: userSafeFields,
+		})
+
+		if (!user)
+			await repository.create({
+				teamId: 1,
+				displayName: `randomUser${i}_team1`,
+				email: `randomUser${i}@team1.com`,
+				password: configuration.initial_user_password,
+				createdAt: Date.now(),
+			})
+	}
+	for (let i = 0; i < 5; i++) {
+		const user = await repository.findOne({
+			where: { email: '`randomUser${i}@team2.com`' },
+			select: userSafeFields,
+		})
+
+		if (!user)
+			await repository.create({
+				teamId: 2,
+				displayName: `randomUser${i}_team2`,
+				email: `randomUser${i}@team2.com`,
+				password: configuration.initial_user_password,
+				createdAt: Date.now(),
+			})
+	}
+}
