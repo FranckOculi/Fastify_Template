fc906c3fa8d2ab0aff1379a993b494a200bcea30
Franck Oculi <100290322+FranckOculi@users.noreply.github.com>
2023-05-31 15:54:11 +0200

    permissionMiddleware Update : add RequestHeaders type

---
+++ src/middlewares/permissionMiddleware.ts
@@ -0,0 +1,25 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { RequestHeaders } from 'src/types/request'
+
+export const permissionVerification = async (
+	req: FastifyRequest<{
+		Headers: RequestHeaders
+		Params: {
+			id: string
+		}
+	}>,
+	res: FastifyReply
+) => {
+	if (
+		typeof req.headers.tokenInfo === 'object' &&
+		req.headers.tokenInfo.authorization.access === 'admin'
+	)
+		return
+	else if (
+		typeof req.headers.tokenInfo === 'object' &&
+		req.headers.tokenInfo.authorization.id !== parseInt(req.params.id)
+	)
+		return res.code(401).send({ message: 'Error token' })
+
+	return
+}
