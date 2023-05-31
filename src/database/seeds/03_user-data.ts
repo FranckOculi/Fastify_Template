f5519d32daa36cf8fb5031eb58b710ef9c78f877
Franck Oculi <100290322+FranckOculi@users.noreply.github.com>
2023-05-31 15:48:34 +0200

    user seed Update : migrate to ts

--- src/database/seeds/03_user-data.ts
+++ src/database/seeds/03_user-data.ts
@@ -1,11 +1,9 @@
 import configuration from '../../config/configuration'
-
 import MigrationsRepository from '../migrations.config/MigrationsRepository'
+import { User } from 'src/types/user'
 
 export const seed = async () => {
-	if (configuration.env === 'production') {
-		return false
-	}
+	// create user if it doesn't exist
 
 	const repository = new MigrationsRepository('users')
 	const userSafeFields = [
@@ -20,64 +18,64 @@ export const seed = async () => {
 		'createdAt',
 	]
 
-	const user1 = await repository.findOne({
+	const user1 = await repository.findOne<Partial<User>>({
 		where: { email: 'a@a.com' },
 		select: userSafeFields,
 	})
 
 	if (!user1)
-		await repository.create({
+		await repository.create<Partial<User>>({
 			teamId: 1,
 			displayName: 'John Doe',
 			email: 'a@a.com',
 			password: configuration.initial_user_password,
 			access: 'manager',
-			createdAt: Date.now(),
+			createdAt: new Date(),
 		})
 
-	const user2 = await repository.findOne({
+	const user2 = await repository.findOne<Partial<User>>({
 		where: { email: 'b@b.com' },
 		select: userSafeFields,
 	})
 
 	if (!user2)
-		await repository.create({
+		await repository.create<Partial<User>>({
 			teamId: 2,
 			displayName: 'Jane Doe',
 			email: 'b@b.com',
 			password: configuration.initial_user_password,
 			access: 'manager',
-			createdAt: Date.now(),
+			createdAt: new Date(),
 		})
 
 	for (let i = 0; i < 5; i++) {
-		const user = await repository.findOne({
+		const user = await repository.findOne<Partial<User>>({
 			where: { email: '`randomUser${i}@team1.com`' },
 			select: userSafeFields,
 		})
 
 		if (!user)
-			await repository.create({
+			await repository.create<Partial<User>>({
 				teamId: 1,
 				displayName: `randomUser${i}_team1`,
 				email: `randomUser${i}@team1.com`,
 				password: configuration.initial_user_password,
-				createdAt: Date.now(),
+				createdAt: new Date(),
 			})
 	}
 	for (let i = 0; i < 5; i++) {
-		const user = await repository.findOne({
+		const user = await repository.findOne<Partial<User>>({
 			where: { email: '`randomUser${i}@team2.com`' },
 			select: userSafeFields,
 		})
 
 		if (!user)
-			await repository.create({
+			await repository.create<Partial<User>>({
 				teamId: 2,
 				displayName: `randomUser${i}_team2`,
 				email: `randomUser${i}@team2.com`,
 				password: configuration.initial_user_password,
-				createdAt: Date.now(),
+				createdAt: new Date(),
 			})
 	}
 }
