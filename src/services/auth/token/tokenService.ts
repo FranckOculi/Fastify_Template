38ed26d7308f9146f8e931bfdffa3ac246b5a49c
Franck Oculi <100290322+FranckOculi@users.noreply.github.com>
2023-05-31 09:23:17 +0200

    tokenService Update : migrate to ts

---
+++ src/services/auth/token/tokenService.ts
@@ -0,0 +1,64 @@
+import { ServiceResponse } from "src/types/Service";
+import Token from "../../../utils/Token";
+import { TokenPayload } from "src/types/Token";
+
+export const refreshTokenService = async (
+  tokenObject: string
+): Promise<ServiceResponse<{ user: number; accessToken: string }>> => {
+  if (!tokenObject) return { error: "Token required", status: 401, data: null };
+  else {
+    try {
+      const token = tokenObject.replace("Bearer ", "");
+
+      if (token) {
+        const decodedToken = <TokenPayload>await Token.verifyToken(token);
+
+        if (!decodedToken)
+          return { error: "Token not valid", status: 401, data: null };
+
+        const accessToken = Token.createToken({
+          id: decodedToken.id,
+          email: decodedToken.email,
+          access: decodedToken.access,
+        });
+
+        return {
+          error: null,
+          status: null,
+          data: {
+            user: decodedToken.id,
+            accessToken,
+          },
+        };
+      } else {
+        return { error: "Token required", status: 401, data: null };
+      }
+    } catch (err) {
+      throw new Error("Unable to verify token");
+    }
+  }
+};
+
+export const verifyTokenService = async (
+  tokenObject: string
+): Promise<ServiceResponse<TokenPayload>> => {
+  if (!tokenObject) return { error: "Token required", status: 401, data: null };
+  else {
+    try {
+      const token = tokenObject.replace("Bearer ", "");
+
+      if (token) {
+        const decodedToken = <TokenPayload>await Token.verifyToken(token);
+
+        if (!decodedToken)
+          return { error: "Token not valid", status: 401, data: null };
+
+        return { error: null, status: null, data: decodedToken };
+      } else {
+        return { error: "Token required", status: 401, data: null };
+      }
+    } catch (err) {
+      throw new Error("Unable to verify token");
+    }
+  }
+};
