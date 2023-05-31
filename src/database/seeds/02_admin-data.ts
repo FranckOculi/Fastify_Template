ed90156ec6ef797c9e740a61144ee20ee7326122
Franck Oculi <100290322+FranckOculi@users.noreply.github.com>
2023-05-31 15:47:45 +0200

    admin seed Update : migrate to ts

--- src/database/seeds/02_admin-data.ts
+++ src/database/seeds/02_admin-data.ts
@@ -1,5 +1,6 @@
 import configuration from '../../config/configuration'
 import MigrationsRepository from '../migrations.config/MigrationsRepository'
+import { User } from 'src/types/user'
 
 export const seed = async () => {
 	// create admin user if it doesn't exist
@@ -17,46 +18,49 @@ export const seed = async () => {
 		'createdAt',
 	]
 
-	const userAdmin1 = await repository.findOne({
+	const userAdmin1: Partial<User> = await repository.findOne({
 		where: { email: 'johnwayne@admin.fr' },
 		select: userSafeFields,
 	})
 
 	if (!userAdmin1) {
-		await repository.create({
+		await repository.create<Partial<User>>({
 			teamId: 1,
 			displayName: 'John Wayne',
 			email: 'johnwayne@admin.fr',
 			password: configuration.initial_admin_password,
 			access: 'admin',
 			phone: '0607080910',
-			createdAt: Date.now(),
+			createdAt: new Date(),
 		})
 	}
 
 	if (userAdmin1 && userAdmin1.access !== 'admin') {
-		await repository.update(userAdmin1.id, {
-			access: 'admin',
-			displayName: 'John Wayne',
-			email: 'johnwayne@admin.fr',
-			updatedAt: Date.now(),
-		})
+		await repository.update<Partial<User>>(
+			{ id: userAdmin1.id },
+			{
+				access: 'admin',
+				displayName: 'John Wayne',
+				email: 'johnwayne@admin.fr',
+				updatedAt: new Date(),
+			}
+		)
 	}
 
-	const userAdmin2 = await repository.findOne({
+	const userAdmin2 = await repository.findOne<Partial<User>>({
 		where: { email: 'chouchou@admin.fr' },
 		select: userSafeFields,
 	})
 
 	if (!userAdmin2) {
-		await repository.create({
+		await repository.create<Partial<User>>({
 			teamId: 2,
 			displayName: 'Chouchou Beignet',
 			email: 'chouchou@admin.fr',
 			password: configuration.initial_admin_password,
 			access: 'admin',
 			phone: '0708091011',
-			createdAt: Date.now(),
+			createdAt: new Date(),
 		})
 	}
 	return null
