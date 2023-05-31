c111d6cfb9ae781e831dbb69e48fb9dbb49955f5
Franck Oculi <100290322+FranckOculi@users.noreply.github.com>
2023-05-31 16:21:37 +0200

    configuration Update : migrate to ts

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -8,12 +8,11 @@ const data = {
 	server_port: parseInt(process.env.SERVER_PORT),
 	app_url: process.env.APP_URL,
 	sqlite_path: process.env.SQLITE_FILENAME,
-	sqlite_path_migrations: process.env.SQLITE_FILENAME_MIGRATIONS,
 	initial_admin_password: process.env.INITIAL_ADMIN_PASSWORD,
 	initial_user_password: process.env.INITIAL_USER_PASSWORD,
 	initial_admin_email: process.env.INITIAL_ADMIN_EMAIL,
 	mail_host: process.env.MAIL_HOST,
-	mail_port: process.env.MAIL_PORT,
+	mail_port: parseInt(process.env.MAIL_PORT),
 	mail_user: process.env.MAIL_USER,
 	mail_password: process.env.MAIL_PASSWORD,
 }
