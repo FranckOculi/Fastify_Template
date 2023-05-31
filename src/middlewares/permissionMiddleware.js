3c2d265102288570724184c668dc065795784f1f
Franck Oculi <100290322+FranckOculi@users.noreply.github.com>
2023-05-31 10:52:36 +0200

    permissionMiddleware Update : migrate to ts

--- src/middlewares/permissionMiddleware.js
+++ src/middlewares/permissionMiddleware.js
@@ -1,4 +1,21 @@
-export const permissionVerification = async (req, res) => {
+import { FastifyReply, FastifyRequest } from 'fastify'
+
+export const permissionVerification = async (
+	req: FastifyRequest<{
+		Headers: {
+			tokenInfo: {
+				authorization: {
+					id: number
+					access: string
+				}
+			}
+		}
+		Params: {
+			id: string
+		}
+	}>,
+	res: FastifyReply
+) => {
 	if (req.headers.tokenInfo.authorization.access === 'admin') return
 	else if (req.headers.tokenInfo.authorization.id !== parseInt(req.params.id))
 		return res.code(401).send({ message: 'Error token' })
