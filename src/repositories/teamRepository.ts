7b4b5fcd4710708333316c0ac59f6b0473e7ab14
Franck Oculi <100290322+FranckOculi@users.noreply.github.com>
2023-06-07 09:24:31 +0200

    teamRepository Update : migrate to ts

--- src/repositories/teamRepository.ts
+++ src/repositories/teamRepository.ts
@@ -1,30 +1,33 @@
-import Repository from '../utils/Repository.js'
+import Repository from '../utils/Repository'
+import { Team } from '../types/team'
 
 const respository = new Repository('teams')
 const teamSafeFields = ['id', 'name', 'description', 'updatedAt', 'createdAt']
 
-export const findAll = async () => {
+export const findAll = async (): Promise<Partial<Team[]>> => {
 	return respository.findAll({ select: teamSafeFields })
 }
 
-export const findById = async (id) => {
+export const findById = async (id: number): Promise<Partial<Team>> => {
 	return respository.findOne({ where: { id }, select: teamSafeFields })
 }
 
-export const findByName = async (name) => {
+export const findByName = async (name: string): Promise<Partial<Team>> => {
 	return respository.findOne({
 		where: { name },
 		select: teamSafeFields,
 	})
 }
 
-export const createTeam = async (data) => {
-	data.createdAt = Date.now()
+export const createTeam = async (
+	data: Partial<Team>
+): Promise<Partial<Team>> => {
+	data.createdAt = new Date()
 
 	const response = await respository.create(data)
 	return findById(response[0])
 }
 
-export const removeTeam = async (id) => {
+export const removeTeam = async (id: number): Promise<number> => {
 	return respository.remove({ where: { id }, select: teamSafeFields })
 }
