9356d610352b35069204790f71dae7b335865f75
Franck Oculi <100290322+FranckOculi@users.noreply.github.com>
2023-06-07 08:51:01 +0200

    tokenService Update : modify imports

--- src/services/auth/token/tokenService.ts
+++ src/services/auth/token/tokenService.ts
@@ -1,64 +1,64 @@
-import { ServiceResponse } from "src/types/Service";
-import Token from "../../../utils/Token";
-import { TokenPayload } from "src/types/Token";
+import Token from '../../../utils/Token'
+import { ServiceResponse } from 'src/types/service'
+import { TokenPayload } from 'src/types/token'
 
 export const refreshTokenService = async (
-  tokenObject: string
+	tokenObject: string
 ): Promise<ServiceResponse<{ user: number; accessToken: string }>> => {
-  if (!tokenObject) return { error: "Token required", status: 401, data: null };
-  else {
-    try {
-      const token = tokenObject.replace("Bearer ", "");
+	if (!tokenObject) return { error: 'Token required', status: 401, data: null }
+	else {
+		try {
+			const token = tokenObject.replace('Bearer ', '')
 
-      if (token) {
-        const decodedToken = <TokenPayload>await Token.verifyToken(token);
+			if (token) {
+				const decodedToken = <TokenPayload>await Token.verifyToken(token)
 
-        if (!decodedToken)
-          return { error: "Token not valid", status: 401, data: null };
+				if (!decodedToken)
+					return { error: 'Token not valid', status: 401, data: null }
 
-        const accessToken = Token.createToken({
-          id: decodedToken.id,
-          email: decodedToken.email,
-          access: decodedToken.access,
-        });
+				const accessToken = Token.createToken({
+					id: decodedToken.id,
+					email: decodedToken.email,
+					access: decodedToken.access,
+				})
 
-        return {
-          error: null,
-          status: null,
-          data: {
-            user: decodedToken.id,
-            accessToken,
-          },
-        };
-      } else {
-        return { error: "Token required", status: 401, data: null };
-      }
-    } catch (err) {
-      throw new Error("Unable to verify token");
-    }
-  }
-};
+				return {
+					error: null,
+					status: null,
+					data: {
+						user: decodedToken.id,
+						accessToken,
+					},
+				}
+			} else {
+				return { error: 'Token required', status: 401, data: null }
+			}
+		} catch (err) {
+			throw new Error('Unable to verify token')
+		}
+	}
+}
 
 export const verifyTokenService = async (
-  tokenObject: string
+	tokenObject: string
 ): Promise<ServiceResponse<TokenPayload>> => {
-  if (!tokenObject) return { error: "Token required", status: 401, data: null };
-  else {
-    try {
-      const token = tokenObject.replace("Bearer ", "");
+	if (!tokenObject) return { error: 'Token required', status: 401, data: null }
+	else {
+		try {
+			const token = tokenObject.replace('Bearer ', '')
 
-      if (token) {
-        const decodedToken = <TokenPayload>await Token.verifyToken(token);
+			if (token) {
+				const decodedToken = <TokenPayload>await Token.verifyToken(token)
 
-        if (!decodedToken)
-          return { error: "Token not valid", status: 401, data: null };
+				if (!decodedToken)
+					return { error: 'Token not valid', status: 401, data: null }
 
-        return { error: null, status: null, data: decodedToken };
-      } else {
-        return { error: "Token required", status: 401, data: null };
-      }
-    } catch (err) {
-      throw new Error("Unable to verify token");
-    }
-  }
-};
+				return { error: null, status: null, data: decodedToken }
+			} else {
+				return { error: 'Token required', status: 401, data: null }
+			}
+		} catch (err) {
+			throw new Error('Unable to verify token')
+		}
+	}
+}
