a4dac7ad22eb58cc23cd05d20a9c81a55883afc8
Franck Oculi <100290322+FranckOculi@users.noreply.github.com>
2023-06-07 08:47:28 +0200

    multipleService.test Update : migrate to ts

--- src/services/user/multiple/multipleService.test.ts
+++ src/services/user/multiple/multipleService.test.ts
@@ -2,7 +2,7 @@ import { fetchAll } from './multipleService'
 
 describe('multipleService', () => {
 	it('should succeed', async () => {
-		const { error, data } = await fetchAll()
+		const { error, status, data } = await fetchAll()
 
 		expect(error).toBe(null)
 		expect(Array.isArray(data)).toBeTruthy()
