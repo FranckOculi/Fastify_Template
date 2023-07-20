e39bc1bbd94879fa83bdef4f3181ada258b4ecf1
Chouchoubeignet <100290322+Chouchoubeignet@users.noreply.github.com>
2023-07-20 18:59:24 +0200

    module declaration type Update : add variables

--- src/types/module.d.ts
+++ src/types/module.d.ts
@@ -1,5 +1,16 @@
 declare namespace NodeJS {
 	export interface ProcessEnv {
+		readonly SERVER_PORT: string
 		readonly TOKEN_SECRET: string
+		readonly APP_URL: string
+		readonly SQLITE_FILENAME: string
+		readonly SQLITE_FILENAME_MIGRATIONS: string
+		readonly INITIAL_ADMIN_PASSWORD: string
+		readonly INITIAL_USER_PASSWORD: string
+		readonly INITIAL_ADMIN_EMAIL: string
+		readonly MAIL_HOST: string
+		readonly MAIL_PORT: string
+		readonly MAIL_USER: string
+		readonly MAIL_PASSWORD: string
 	}
 }
